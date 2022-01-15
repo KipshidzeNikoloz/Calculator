008236c01a4d26ee930781ac9da49a1aaedc3055
KipshidzeNikoloz <kipshidzenikoloz@gmail.com>
2022-01-15 23:09:35 +0400

    adding number functions and clear and remove last number functions

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+
+
+let numbers = [];
+
+
+//HTML Eventlisteners
+
+document.querySelectorAll(".number").forEach(function(ele){
+    ele.addEventListener('click', function(e){
+        var dataKey = this.dataset.num;  
+        numbers.push(dataKey);
+        displayUpdate();
+    });
+})
+
+function displayUpdate(){
+    if (Array.isArray(numbers) && !numbers.length) {
+        numberDisplay.textContent = '0';
+    } else{ 
+        var numbersToDisplay = numbers.join("");
+        numberDisplay.textContent = numbersToDisplay;
+    }
+    
+}
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', clearNumbers)
+const removeLast = document.querySelector('.removeLast')
+removeLast.addEventListener('click', removeLastNumber)
+const numberDisplay = document.querySelector('.numberDisplay')
+const additionButton = document.querySelector('.addition')
+const subtractionButton = document.querySelector('.subtraction')
+const multiplicationButton = document.querySelector('.multiplication')
+const divisionButton = document.querySelector('.division')
+const enterButton = document.querySelector('.enter')
+displayUpdate();
+
+
+function clearNumbers(){
+    numbers = [];
+    displayUpdate();
+}
+
+function removeLastNumber(){
+    numbers.pop();
+    displayUpdate();
+}
+
 function addition(...num) {
     return num.reduce((a, b) => a + b)
 }
@@ -18,3 +66,4 @@ function operator(operate, a, b) {
     else if (operate === '/') return divide(a, b)
     else return NaN
 }
+
