0af77608b68c8d0c0dcaeb35ce0b1170f651498c
KipshidzeNikoloz <kipshidzenikoloz@gmail.com>
2022-01-20 03:09:38 +0400

    Added Javascript functionality, calculator works, rounds to 5th decimal point, keyboard integration tbd

--- script.js
+++ script.js
@@ -1,69 +1,193 @@
+// HTML References  
 
 
-let numbers = [];
+const clear = document.querySelector('.clear');
+const removeLast = document.querySelector('.removeLast')
+const numberDisplay = document.querySelector('.numberDisplay')
+const enter = document.querySelector('.enter')
+const lastNumberDisplay = document.querySelector('.lastNumberDisplay')
+const enterButton = document.querySelector('.enter')
+const operators = document.querySelectorAll('.operator')
+const decimal = document.querySelector('.decimal')
 
 
-//HTML Eventlisteners
+// Starting state 
+
+
+let firstOperand = '';
+let secondOperand = '';
+let activeOperation = null;
+let moveNumberUp = false
+numberDisplay.textContent = '0';
+
+
+
+// Number and Operator Eventlisteners
 
 document.querySelectorAll(".number").forEach(function(ele){
     ele.addEventListener('click', function(e){
-        var dataKey = this.dataset.num;  
-        numbers.push(dataKey);
-        displayUpdate();
+        if (numberDisplay.textContent != 'Why would you do this') {
+            appendNumber(e.target.textContent)
+            return;
+        }
+        clearDisplay();
+        appendNumber(e.target.textContent)
     });
 })
 
-function displayUpdate(){
-    if (Array.isArray(numbers) && !numbers.length) {
-        numberDisplay.textContent = '0';
-    } else{ 
-        var numbersToDisplay = numbers.join("");
-        numberDisplay.textContent = numbersToDisplay;
-    }
+document.querySelectorAll(".operator").forEach(function(ele){
+    ele.addEventListener('click', function(e){
+           if (numberDisplay.textContent != 'Why would you do this') 
+           setOperation(e.target.textContent);
+    });
+})
+
+// DISPLAY OPERATIONS
+
+
+const appendNumber = (num) => {
+    if (numberDisplay.textContent === '0' || moveNumberUp)  //checks if 0 or  lastNumberDisplay is displayed 
+         displayUpdate()
+    numberDisplay.textContent += num;
+}
+
+const displayUpdate = () => {
+    numberDisplay.textContent = ''
+    moveNumberUp = false;
+}
+
+const clearDisplay = () => {
+    numberDisplay.textContent = '0';
+    lastNumberDisplay.textContent = '';     
+    firstOperand = '';          //
+    secondOperand = '';         //  resets everything
+    activeOperation = null;     //
     
 }
 
-const clear = document.querySelector('.clear');
-clear.addEventListener('click', clearNumbers)
-const removeLast = document.querySelector('.removeLast')
-removeLast.addEventListener('click', removeLastNumber)
-const numberDisplay = document.querySelector('.numberDisplay')
-const additionButton = document.querySelector('.addition')
-const subtractionButton = document.querySelector('.subtraction')
-const multiplicationButton = document.querySelector('.multiplication')
-const divisionButton = document.querySelector('.division')
-const enterButton = document.querySelector('.enter')
-displayUpdate();
-
-
-function clearNumbers(){
-    numbers = [];
-    displayUpdate();
+const removeLastNumber = () => {
+    if (numberDisplay.textContent === 'Why would you do this') {    //snark
+        clearDisplay();
+        return;
+    } 
+    numberDisplay.textContent = numberDisplay.textContent.toString().slice(0, -1);    
 }
 
-function removeLastNumber(){
-    numbers.pop();
-    displayUpdate();
+
+const appendDecimal = () => {
+    if (moveNumberUp) {
+        displayUpdate();
+    }
+    if (numberDisplay.textContent === '') {
+        numberDisplay.textContent = '0'
+    }
+    if (numberDisplay.textContent.includes('.')) return;
+    
+    numberDisplay.textContent += '.'
 }
 
-function addition(...num) {
-    return num.reduce((a, b) => a + b)
-}
-function subtraction(...num) {
-    return num.reduce((a, b) => a - b)
-}
-function multiply(...num) {
-    return num.reduce((a, b) => a * b);
-  };
-function divide(...num) {
-    return num.reduce((a, b) => a / b)
+// MATH OPERATIONS
+
+const setOperation = (operator) => {
+    if (activeOperation !== null) operate()
+    firstOperand = numberDisplay.textContent;
+    activeOperation = operator;
+    lastNumberDisplay.textContent = `${firstOperand} ${activeOperation}`
+    moveNumberUp = true;
 }
 
-function operator(operate, a, b) {
-    if (operate === '+') return addition(a, b);
-    else if (operate === '-') return subtraction(a, b);
-    else if (operate === '*') return multiply(a, b)
-    else if (operate === '/') return divide(a, b)
-    else return NaN
+const operate = () => {
+    if (activeOperation === null || moveNumberUp) return; 
+    if (activeOperation === '÷' && numberDisplay.textContent === '0') {
+        numberDisplay.textContent = 'Why would you do this';
+        lastNumberDisplay.textContent = ''
+        return;
+    }
+    secondOperand = numberDisplay.textContent;
+    numberDisplay.textContent = rounding(operator(activeOperation, firstOperand, secondOperand))    
+    lastNumberDisplay.textContent = `${firstOperand} ${activeOperation} ${secondOperand} =`
+    activeOperation = null;
 }
 
+const addition = (a, b) => {
+    return a + b
+}
+const subtraction = (a, b) => {
+    return a - b
+}
+const multiply = (a, b) => {
+    return a * b
+};
+const divide = (a, b) => {
+    return a / b
+}
+
+const operator = (operator, a, b) => {
+    a = Number(a)
+    b = Number(b)
+
+    switch (operator) {
+        case '+':
+            return addition(a, b)
+        case '-':
+            return subtraction(a, b)
+        case '*':
+            return multiply(a, b) 
+        case '÷':
+            if (b === 0) return null
+            else return divide(a, b)    
+        default: 
+            return null;       
+    }
+}
+
+const rounding = (num) => {
+    return Math.round(num * 10000) / 10000
+}
+
+
+// Event Listeners 
+clear.addEventListener('click', clearDisplay);
+removeLast.addEventListener('click', removeLastNumber);
+enter.addEventListener('click', operate)
+decimal.addEventListener('click', appendDecimal)
+
+// Keyboard Integration 
+
+window.addEventListener("keydown", function (e) {
+    setOperation(keyboardOperators(e.key));
+    
+    switch (e) {
+        case '+':
+            return '+';
+            
+    }
+
+})
+
+
+//     if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
+//     if (e.key === '.') appendDecimal;
+//     if (e.key === '=' || e.key === 'Enter') operate;
+//     if (e.key === 'Backspace') removeLastNumber;
+//     if (e.key === 'Escape') clearDisplay;
+//     if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+//       setOperation(keyboardOperators(e.key))
+// })
+// 
+const keyboardOperators = (operator) => {
+    if (operator.defaultPrevented) {
+        return; 
+    }
+    switch (operator) {
+        case "+":
+            return '+';
+        case '-':
+            return '-';
+        case '*':
+            return '*';
+        case '/':
+            return '/';
+    }
+ operator.preventDefault();
+}
